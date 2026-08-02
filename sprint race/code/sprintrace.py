6cac3c6e56671689443581f3832dcddaeb6c23de
yash <yashajagiya1245@gmail.com>
2026-08-03 02:29:11 +0530

    Auto-update sprint race JSON data

--- sprint race/code/sprintrace.py
+++ sprint race/code/sprintrace.py
@@ -203,7 +203,7 @@ if __name__ == "__main__":
     
     if url:
         script_dir = os.path.dirname(os.path.abspath(__file__))
-        output_file = os.path.join(os.path.dirname(script_dir), 'sprint rcae result.json')
+        output_file = os.path.join(os.path.dirname(script_dir), 'sprint_race_result.json')
         scrape_f1_race_result(url, output_file, circuit_id=circuit_id)
         # Push to github automatically
         push_to_git('sprint race')
